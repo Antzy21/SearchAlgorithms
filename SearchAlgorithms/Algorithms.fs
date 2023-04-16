a969d566e8250228962d1c88ba9d741795d2b43b
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-16 18:19:56 +0100

    Add project files.

---
+++ SearchAlgorithms/Algorithms.fs
@@ -0,0 +1,27 @@
+﻿namespace SearchAlgorithms
+
+open System.Numerics
+
+module Algorithms =
+    
+    /// Basic min/max function
+    let rec minMax
+        (getNodesFromParent: 'Node -> 'Node list)
+        (evaulationFunction: 'Node -> 'EvalValue)
+        (depth: int) (isMaxing: bool) (parentNode: 'Node) : 'EvalValue when IMinMaxValue<'EvalValue> =
+        if depth = 0 then
+            evaulationFunction parentNode
+        else
+            let nodes = getNodesFromParent parentNode
+            match nodes with
+            | [] ->
+                evaulationFunction parentNode
+            | nodes ->
+                let minMaxFunc = if isMaxing then max else min
+                let initValue = if isMaxing then 'EvalValue.MaxValue else 'EvalValue.MaxValue
+                List.fold (fun currentOptimalEval node ->
+                    let nodeEval =
+                        minMax getNodesFromParent evaulationFunction (depth-1) (not isMaxing) node
+                    minMaxFunc currentOptimalEval nodeEval
+                ) initValue nodes   
+        
