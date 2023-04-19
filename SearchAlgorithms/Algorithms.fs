3b83fa9d5950c88b90b1872385be6cd103278115
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-19 08:04:48 +0100

    Restructure MinMax to return move and evaluation

--- SearchAlgorithms/Algorithms.fs
+++ SearchAlgorithms/Algorithms.fs
@@ -6,22 +6,26 @@ module Algorithms =
     
     /// Basic min/max function
     let rec minMax
-        (getNodesFromParent: 'Node -> 'Node list)
-        (evaulationFunction: 'Node -> 'EvalValue)
-        (depth: int) (isMaxing: bool) (parentNode: 'Node) : 'EvalValue when IMinMaxValue<'EvalValue> =
+        (getNodesFromParent: 'Node -> moveAndNode<'Move, 'Node> list)
+        (evaulationFunction: moveAndNode<'Move, 'Node> -> 'EvalValue)
+        (depth: int) (isMaxing: bool) (parentNode: moveAndNode<'Move, 'Node>) : moveAndEvaluation<'Move,'EvalValue> =
         if depth = 0 then
-            evaulationFunction parentNode
+            {move = parentNode.move; eval = evaulationFunction parentNode}
         else
-            let nodes = getNodesFromParent parentNode
-            match nodes with
+            let movesAndNodeList = getNodesFromParent parentNode.node
+            match movesAndNodeList with
             | [] ->
-                evaulationFunction parentNode
-            | nodes ->
-                let minMaxFunc = if isMaxing then max else min
+                {move = parentNode.move; eval = evaulationFunction parentNode}
+            | movesAndNodeList ->
+
+                let minMaxFunc = if isMaxing then MoveAndEvaluation.max else MoveAndEvaluation.min
                 let initValue = if isMaxing then 'EvalValue.MaxValue else 'EvalValue.MaxValue
-                List.fold (fun currentOptimalEval node ->
-                    let nodeEval =
-                        minMax getNodesFromParent evaulationFunction (depth-1) (not isMaxing) node
-                    minMaxFunc currentOptimalEval nodeEval
-                ) initValue nodes   
+                let initMove = null // This will always be replaced with a value
+                let moveAndEval = {move = initMove; eval = initValue}
+
+                List.fold (fun currentOptimalMoveAndEval moveAndNode ->
+                    let nodeMoveAndEval =
+                        minMax getNodesFromParent evaulationFunction (depth-1) (not isMaxing) moveAndNode
+                    minMaxFunc currentOptimalMoveAndEval nodeMoveAndEval
+                ) moveAndEval movesAndNodeList
         
