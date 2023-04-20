e3201df81ccc20f1fe408d71379077eb25d962ce
Anthony Dunford <Anthony@Dunford.uk.com>
2023-04-20 10:40:56 +0100

    Set move to be optional

--- SearchAlgorithms/Algorithms.fs
+++ SearchAlgorithms/Algorithms.fs
@@ -19,9 +19,9 @@ module Algorithms =
     /// Basic min/max function
     let rec minMax
         (getNodesFromParent: 'Node -> ('Move * 'Node) list)
-        (evaulationFunction: 'Move -> 'Node -> 'EvalValue)
-        (depth: int) (isMaxing: bool) (previousMove: 'Move) (parentNode: 'Node)
-        : 'Move * 'EvalValue when IMinMaxValue<'EvalValue> =
+        (evaulationFunction: 'Move option -> 'Node -> 'EvalValue)
+        (depth: int) (isMaxing: bool) (previousMove: 'Move option) (parentNode: 'Node)
+        : 'Move option * 'EvalValue when IMinMaxValue<'EvalValue> =
         if depth = 0 then
             previousMove, evaulationFunction previousMove parentNode
         else
@@ -32,11 +32,11 @@ module Algorithms =
             | movesAndNodeList ->
 
                 let initEval = if isMaxing then 'EvalValue.MaxValue else 'EvalValue.MaxValue
-                let initMove = fst movesAndNodeList.Head
+                let initMove = None
 
                 List.fold (fun optimalMoveAndEval (move, node) ->
                     let _, eval =
-                        minMax getNodesFromParent evaulationFunction (depth-1) (not isMaxing) move node
-                    getMinOrMaxOfMoveAndEval isMaxing optimalMoveAndEval (move, eval)                     
+                        minMax getNodesFromParent evaulationFunction (depth-1) (not isMaxing) (Some move) node
+                    getMinOrMaxOfMoveAndEval isMaxing optimalMoveAndEval (Some move, eval)                     
                 ) (initMove, initEval) movesAndNodeList
         
