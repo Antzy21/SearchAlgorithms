ec61c906ae1f29f430763e737c35acb948af46ca
Anthony Dunford <Anthony@Dunford.uk.com>
2023-05-09 23:09:55 +0100

    Add alphabeta pruning function

--- SearchAlgorithms/Algorithms.fs
+++ SearchAlgorithms/Algorithms.fs
@@ -39,4 +39,50 @@ module Algorithms =
                         minMax getNodesFromParent evaulationFunction (depth-1) (not isMaxing) (Some move) node
                     getMinOrMaxOfMoveAndEval isMaxing optimalMoveAndEval (Some move, eval)                     
                 ) (initMove, initEval) movesAndNodeList
-        
+    
+    let rec private alphaBetaPruning 
+        (getNodesFromParent: 'Node -> ('Move * 'Node) list)
+        (evaulationFunction: 'Move option -> 'Node -> 'EvalValue)
+        (depth: int) (isMaxing: bool) (previousMove: 'Move option) (parentNode: 'Node)
+        (alpha: 'EvalValue) (beta: 'EvalValue)
+        : 'Move option * 'EvalValue * 'EvalValue * 'EvalValue when IMinMaxValue<'EvalValue> =
+        if depth = 0 then
+            previousMove, evaulationFunction previousMove parentNode, alpha, beta
+        else
+            let movesAndNodeList = getNodesFromParent parentNode
+            match movesAndNodeList with
+            | [] ->
+                previousMove, evaulationFunction previousMove parentNode, alpha, beta
+            | movesAndNodeList ->
+
+                let initEval = if isMaxing then 'EvalValue.MinValue else 'EvalValue.MaxValue
+                let initMove = None
+
+                List.fold (fun (optimalMove, optimalEval, skip, alpha, beta) (move, node) ->
+                    if skip then
+                        (optimalMove, optimalEval, skip, alpha, beta)
+                    else
+                        let _, eval, alpha, beta =
+                            alphaBetaPruning getNodesFromParent evaulationFunction (depth-1) (not isMaxing) (Some move) node alpha beta
+                        let (newOptimalEval, newOptimalMove) = getMinOrMaxOfMoveAndEval isMaxing (optimalMove, optimalEval) (Some move, eval)                     
+                        
+                        if isMaxing then
+                            let newAlpha = max alpha eval
+                            let skip = beta <= alpha
+                            (newOptimalEval, newOptimalMove, skip, newAlpha, beta)
+                        else
+                            let newBeta = min beta eval
+                            let skip = beta <= alpha
+                            (newOptimalEval, newOptimalMove, skip, alpha, newBeta)
+                        
+                ) (initMove, initEval, false, alpha, beta) movesAndNodeList
+                |> fun (optimalMove, optimalEval, skip, alpha, beta) -> (optimalMove, optimalEval, alpha, beta)
+
+
+    let minMaxAbPruning
+        (getNodesFromParent: 'Node -> ('Move * 'Node) list)
+        (evaulationFunction: 'Move option -> 'Node -> 'EvalValue)
+        (depth: int) (isMaxing: bool) (previousMove: 'Move option) (parentNode: 'Node)
+        : 'Move option * 'EvalValue when IMinMaxValue<'EvalValue> =
+            alphaBetaPruning getNodesFromParent evaulationFunction depth isMaxing previousMove parentNode 'EvalValue.MinValue 'EvalValue.MaxValue
+            |> fun (optimalMove, optimalEval, alpha, beta) -> (optimalMove, optimalEval)
