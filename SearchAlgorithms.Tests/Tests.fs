32a429f6b41d778a5968ea339553bbef550d0492
Anthony Dunford <Anthony@Dunford.uk.com>
2023-05-11 11:06:20 +0100

    Add proof that ab pruning is faster

--- SearchAlgorithms.Tests/Tests.fs
+++ SearchAlgorithms.Tests/Tests.fs
@@ -98,3 +98,27 @@ let ``minMax and ab pruning return same result`` (seed: int) =
     let resultFromMinMax = Algorithms.minMax getNodesFromParent evaluationFunction depth true None []
     let resultFromAbPrune = Algorithms.minMaxAbPruning getNodesFromParent evaluationFunction depth true None []
     Assert.Equal(resultFromMinMax, resultFromAbPrune)
+
+// This avoids having to generate the game tree before hand, which can be expensive,
+// but it also maintains unique, random and seeded values at each node,
+// by using the branch list to seed a Random object.
+let evaluationFunctionForProceduralyGeneratedGame (move: move option) (node: node) : int =
+    node
+    |> List.fold (fun acc i ->
+        acc + $"{i}"
+    ) ""
+    |> int
+    |> (fun i -> new Random(i))
+    |> fun r -> r.Next()
+
+[<Fact>]
+let ``Ab is fast`` () =
+    let branchCount = 7
+    let depth = 8
+    let getNodesFromParent = getNodesFromParentWithBranchCount branchCount
+    
+    // normal min max takes ~30 seconds
+    let resultFromMinMax = Algorithms.minMaxAbPruning getNodesFromParent evaluationFunctionForProceduralyGeneratedGame depth true None []
+    
+    Assert.True
+    
